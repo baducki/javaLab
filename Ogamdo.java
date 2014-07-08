2f1dbe9d2fd4c234f577dfe86d21cfef506d3f44
YUN YOUNGGI <YG@YUNui-MacBook-Pro.local>
2014-07-09 04:56:08 +0900

    오감도구현

--- Ogamdo.java
+++ Ogamdo.java
@@ -1,25 +1,22 @@
 class Ogamdo {
 	public static void main (String[] args)
 	{
-		int i = 0, j = 0;
+		int i = 0;
 
 		String ahae1 = "무서운아해";
 		String ahae2 = "무서워하는아해";
-		String musup = "무섭다고그리오.";
 
-		System.out.println("\t오감도-시제1호");
-		System.out.println("\t\t\t이상\n");
-		System.out.println(" 13인의아해가도로로질주하오.\n (길은막다른골목길이적당하오.)\n");
+		System.out.println("\n\t오감도-시제1호\n\t\t\t이상\n\n 13인의아해가도로로질주하오.\n (길은막다른골목길이적당하오.)\n");
 
 		for ( ; i < 13; ){
-			System.out.print(" 제" +  ++i + "의아해");
+			char josa = ' ';
 
 			if (i%10 == 1)
-				System.out.print("가");
+				josa = '가';
 			else
-				System.out.print("도");
+				josa = '도';
 
-			System.out.println(musup);
+			System.out.println(" 제" +  ++i + "의아해" + josa + "무섭다고그리오.");
 			
 			if (i%10 == 0)
 				System.out.println();
@@ -27,12 +24,19 @@ class Ogamdo {
 
 		System.out.println(" " + i + "인의아해는" + ahae1 + "와" + ahae2 + "와그러케뿐이모엿소.\n (다른사정은없는것이차라리나앗소)\n");
 
-		for (j = 0; j < 2; ){
-			System.out.println(" 그중에" + ++j + "인의아해가" + ahae1 + "라도좃소.");
-		}
+		for (int count = 0, j = 0; count < 4; count++){
+			String ahae = "";
 
-		for ( ; j > 0; j--){
-			System.out.println(" 그중에" + j + "인의아해가" + ahae2 + "라도좃소.");
+			if (count < 2){
+				j++;
+				ahae = ahae1;				
+			}
+			else if (count > 2){
+				j--;
+				ahae = ahae2;
+			}
+
+			System.out.println(" 그중에" + j + "인의아해가" + ahae + "라도좃소.");
 		}
 
 		System.out.println(" (길은뚫닌골목이라도적당하오.)\n\n " + i + "인의아해가도로로질주하지아니하여도좃소.");
